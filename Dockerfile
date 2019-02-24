c78658a4f243d9aabec3e579a9c3d1be9856ec02
huangkai <huankai@139.com>
2019-02-24 22:36:38 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,19 @@
 FROM docker.io/java:8-jre-alpine
 
-COPY target/test-0.0.1-SNAPSHOT.jar /app/test-0.0.1-SNAPSHOT.jar
+#指定作者
+MAINTAINER huangkai huankai@139.com
 
+# 定义变量
+ARG SERVER_NAME=test-0.0.1-SNAPSHOT.jar
+
+COPY target/$SERVER_NAME /app/$SERVER_NAME
+
+# 指定工作目录
 WORKDIR /app
 
-ENTRYPOINT ["java", "-jar","test-0.0.1-SNAPSHOT.jar"]
+# 只是声明了容器应该打开的端口并没有实际上将它打开
+# 需要使用-p或者-P中指定要映射的端口，容器端口才能映射出去
+EXPOSE [8780]
+
+# 执行命令
+ENTRYPOINT ["java","-jar","$SERVER_NAME"]
--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,12 +2,20 @@ package com.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "success";
+    }
 
 }
